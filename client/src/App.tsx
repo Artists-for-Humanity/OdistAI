54986a9ed39c0aba80f50fb1f61a2735c4c27431
okitsqwerty <chen.luk@northeastern.edu>
2023-07-12 13:42:37 -0400

    abstract textarea into its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import TextInput from "./Components/TextInputBox";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <textarea
+      {/* <textarea
         id="message"
         className="resize-none block w-96 h-80 mb-8 p-2.5 w-full text-sm text-gray-900 drop-shadow-2xl bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Enter text..."
-      ></textarea>
+      ></textarea> */}
+      <TextInput></TextInput>
       <div className="items-center justify-center hidden col-span-1 space-x-2 sm:flex">
         <button
           type="button"
@@ -23,14 +25,14 @@ function App() {
           type="button"
           className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         >
-          Good
+          Accept
         </button>
 
         <button
           type="button"
           className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         >
-          Bad
+          Reject
         </button>
       </div>
     </div>
